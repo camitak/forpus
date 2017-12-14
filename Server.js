59c69d8f56ae3472b56839b06bbea93ec05b3b9f
Camila Takemoto <ctakemoto@ctakemotob.home>
2017-12-14 10:18:45 -0200

    pasta dist

--- Server.js
+++ Server.js
@@ -1,11 +1,6 @@
-// const express = require('express');
-const http = require('http');
-const path = require('path');
-var cors = require('cors')
-
-
 const express = require('express');
 const app = express();
+
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -24,6 +19,46 @@ const forceSSL = function() {
 // to use the forceSSL
 // middleware
 app.use(forceSSL());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// // const express = require('express');
+// const http = require('http');
+// const path = require('path');
+// var cors = require('cors')
+
+
+// const express = require('express');
+// const app = express();
+// // If an incoming request uses
+// // a protocol other than HTTPS,
+// // redirect that request to the
+// // same url but with HTTPS
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
 
 
 
